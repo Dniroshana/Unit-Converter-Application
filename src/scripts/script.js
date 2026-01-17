89031359b604323d98a827dd16a26fcaf00dd7e6
sasmitha <sasmitharusiru4@gmail.com>
2026-01-17 16:43:45 +0000

    feat: Add conversion constants for length and weight

---
+++ src/scripts/script.js
@@ -0,0 +1,20 @@
+const conversions = {
+            length: {
+                meter: 1,
+                kilometer: 0.001,
+                centimeter: 100,
+                millimeter: 1000,
+                mile: 0.000621371,
+                yard: 1.09361,
+                foot: 3.28084,
+                inch: 39.3701
+            },
+            weight: {
+                kilogram: 1,
+                gram: 1000,
+                milligram: 1000000,
+                ton: 0.001,
+                pound: 2.20462,
+                ounce: 35.274
+            }
+        };
